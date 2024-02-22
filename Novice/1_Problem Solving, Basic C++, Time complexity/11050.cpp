a14706c982eafa552e5bf797deae69db02ce21a0
YoungEun <y_e_99@ewhain.net>
2024-02-23 01:06:30 +0900

    update: Advanced/Problem Solving

--- Novice/1_Problem Solving, Basic C++, Time complexity/11050.cpp
+++ Novice/1_Problem Solving, Basic C++, Time complexity/11050.cpp
@@ -2,11 +2,11 @@
 
 using namespace std;
 
-int factorial(int n){
-    if(n <= 1)
+int factorial(int n) {
+    if (n <= 1)
         return 1;
     else
-        return n * factorial(n-1);
+        return n * factorial(n - 1);
 }
 
 int main() {
