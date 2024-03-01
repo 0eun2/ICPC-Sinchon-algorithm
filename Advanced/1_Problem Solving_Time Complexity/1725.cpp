938b2213bf24dcf60c98be952307e8303256c99f
YoungEun <y_e_99@ewhain.net>
2024-03-01 20:16:44 +0900

    update: Advanced/Problem Solving

--- Advanced/1_Problem Solving_Time Complexity/1725.cpp
+++ Advanced/1_Problem Solving_Time Complexity/1725.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<stack>
+#include<vector>
 
 using namespace std;
 
@@ -8,7 +10,30 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
 
+    //입력
+    int N, ans = 0;
+    cin >> N;
 
+    stack<int> stack;
+    vector<int> h(N + 2);   //높이
+
+    for (int i = 1; i <= N; i++) {
+        cin >> h[i];
+    }
+
+    //연산
+    stack.push(0);
+    for (int i = 1; i <= N + 1; i++) {
+        while (!stack.empty() && h[stack.top()] > h[i]) {
+            int check = stack.top();
+            stack.pop();
+            ans = max(ans, h[check] * (i - stack.top() - 1));
+        }
+        stack.push(i);
+    }
+
+    //출력
+    cout << ans;
 
     return 0;
 }
