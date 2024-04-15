6c84ba9d229cdf2845bacbb47ff50be4424cb2a4
YoungEun <y_e_99@ewhain.net>
2024-04-15 12:21:54 +0900

    add

--- Novice/3_Basic Math(Number Theory)/9020.cpp
+++ Novice/3_Basic Math(Number Theory)/9020.cpp
@@ -6,12 +6,12 @@ using namespace std;
 vector<bool> is_prime; //소수: true
 
 void isPrime(int n) {
-    is_prime.assign(2 * n + 1, true);
+    is_prime.assign(n, true);
     is_prime[0] = is_prime[1] = false;
-    for (int i = 2; i <= 2 * n; i++) {
+    for (int i = 2; i <= n; i++) {
         if (!is_prime[i])
             continue;
-        for (int j = i * i; j <= 2 * n; j += i) {
+        for (int j = i * i; j <= n; j += i) {
             if (!is_prime[j])
                 continue;
             is_prime[j] = false;
@@ -21,14 +21,17 @@ void isPrime(int n) {
 
 int main() {
     int T;
+    cin >> T;
 
     while (T--) {
         int n;
         cin >> n;
         isPrime(n);
         for (int i = n / 2; i >= 2; i--) {
-            if (is_prime(i) && is_prime(n - i))
-                
+            if (is_prime[i] && is_prime[n - i]) {
+                cout << i << ' ' << n - i << '\n';
+                break;
+            }
         }
     }
 
